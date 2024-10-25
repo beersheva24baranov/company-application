f4550f7ca633b7e44ba6147ba95a3dd0f706c7d6
AlexanderBaranov <avbc@mail.ru>
2024-10-25 21:19:20 +0300

    hw

--- company-appl/src/main/java/employees/CompanyItems.java
+++ company-appl/src/main/java/employees/CompanyItems.java
@@ -3,10 +3,52 @@ package employees;
 import view.*;
 
 public class CompanyItems {
-  //TODO
+  private static Company company;
   public static Item[] getItems(Company company) {
     //TODO
     //item for each serverce method
-     throw new UnsupportedOperationException();
-  }
+    Item[] items = {
+    Item.of("Add Employee", CompanyItems::addEmployee),
+    Item.of("Display Employee Data", CompanyItems::displayEmployee),
+    Item.of("Fire Employee", CompanyItems::fireEmployee),
+    Item.of("Department Salary Budget", CompanyItems::displayDepartmentSalaryBudget),
+    Item.of("List of Departments", CompanyItems::displayDepartments),
+    Item.of("Display Managers with Most Factor", CompanyItems::displayManagersMostFactor),
+    Item.ofExit()
+};
+
+return items;
+};
+
+private static void addEmployee(InputOutput io) {
+Menu menu = new Menu("Select Employee type", EmployeeItems.getItems(company));
+menu.perform(io);
+}
+
+private static void displayEmployee(InputOutput io) {
+long id = io.readLong("Enter ID:", null);
+Employee empl = company.getEmployee(id);
+io.writeLine(empl == null ? "Employee not found" : empl);
+}
+
+private static void fireEmployee(InputOutput io) {
+long id = io.readLong("Enter ID:", null);
+company.removeEmployee(id);
+io.writeLine("Employee fired");
+}
+
+private static void displayDepartmentSalaryBudget(InputOutput io) {
+String department = io.readString("Enter department:");
+io.writeLine(company.getDepartmentBudget(department));
+}
+
+private static void displayDepartments(InputOutput io) {
+io.writeLine(String.join(", ", company.getDepartments()));
+}
+
+private static void displayManagersMostFactor(InputOutput io) {
+for (Manager manager : company.getManagersWithMostFactor()) {
+io.writeLine(manager);
+}
+}
 }
---
+++ company-appl/src/main/java/employees/EmployeeItems.java
@@ -0,0 +1,150 @@
+package employees;
+import employees.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+import view.InputOutput;
+import view.Item;
+public class EmployeeItems {
+    final static int MIN_SALARY = 5000;
+    final static int MAX_SALARY = 30000;
+    final static String[] DEPARTMENTS = { "QA", "Audit", "Development", "Management" };
+    final static long MIN_ID = 100000;
+    final static long MAX_ID = 999999;
+    final static int MIN_FACTOR = 1;
+    final static int MAX_FACTOR = 4;
+    final static int MIN_WAGE = 50;
+    final static int MAX_WAGE = 1000;
+    final static int MIN_HOURS = 1;
+    final static int MAX_HOURS = 160;
+    final static float MIN_PERCENT = 5;
+    final static float MAX_PERCENT = 80;
+    final static long MIN_SALES = 0;
+    final static long MAX_SALES = 1000000;
+
+    private static Company company;
+
+    public static Item[] getItems(Company company) {
+        EmployeeItems.company = company;
+
+        Item[] items = {
+                Item.of("Hire Employee", EmployeeItems.addEmployee(EmployeeItems::enterEmployee)),
+                Item.of("Hire Wage Employee", EmployeeItems.addEmployee(EmployeeItems::enterWageEmployee)),
+                Item.of("Hire Sales Person", EmployeeItems.addEmployee(EmployeeItems::enterSalesPerson)),
+                Item.of("Hire Manager", EmployeeItems.addEmployee(EmployeeItems::enterManager)),
+                Item.ofExit()
+        };
+
+        return items;
+    }
+
+    private static Consumer<InputOutput> addEmployee(Function<InputOutput, Employee> reader) {
+        return i -> company.addEmployee(reader.apply(i));
+    }
+
+    public static Employee enterEmployee(InputOutput io) {
+        return new Employee(
+            readId(io),
+            readSalary(io),
+            readDepartment(io)
+        );
+    }
+
+    public static WageEmployee enterWageEmployee(InputOutput io) {
+        Employee employee = enterEmployee(io);
+        return new WageEmployee(
+            employee.getId(),
+            employee.getSalary(),
+            employee.getDepartment(),
+            readWage(io),
+            readHours(io)
+        );
+    }
+
+    public static SalesPerson enterSalesPerson(InputOutput io) {
+        WageEmployee employee = enterWageEmployee(io);
+        return new SalesPerson(
+            employee.getId(),
+            employee.getSalary(),
+            employee.getDepartment(),
+            0, 0, readPercent(io),
+            readSales(io)
+        );
+    }
+
+    public static Manager enterManager(InputOutput io) {
+        Employee employee = enterEmployee(io);
+        return new Manager(
+            employee.getId(),
+            employee.getSalary(),
+            employee.getDepartment(),
+            readFactor(io)
+        );
+    }
+
+    private static long readId(InputOutput io) {
+        return io.readNumberRange(
+                "Enter ID:",
+                "Wrong format for ID",
+                MIN_ID,
+                MAX_ID).longValue();
+    }
+
+    private static int readSalary(InputOutput io) {
+        return io.readNumberRange(
+                "Enter salary:",
+                "Wrong format for salary",
+                MIN_SALARY,
+                MAX_SALARY).intValue();
+    }
+
+    private static String readDepartment(InputOutput io) {
+        return io.readStringOptions(
+                "Enter department:",
+                "Wrong format for department",
+                new HashSet<String>(Arrays.asList(DEPARTMENTS)));
+    }
+
+    private static float readFactor(InputOutput io) {
+        return io.readNumberRange(
+                "Enter factor:",
+                "Wrong format for factor",
+                MIN_FACTOR,
+                MAX_FACTOR).floatValue();
+    }
+
+    private static int readWage(InputOutput io) {
+        return io.readNumberRange(
+                "Enter wage:",
+                "Wrong format for wage",
+                MIN_WAGE,
+                MAX_WAGE).intValue();
+    }
+
+    private static int readHours(InputOutput io) {
+        return io.readNumberRange(
+                "Enter hours:",
+                "Wrong format for hours",
+                MIN_HOURS,
+                MAX_HOURS).intValue();
+    }
+
+    private static float readPercent(InputOutput io) {
+        return io.readNumberRange(
+                "Enter percent:",
+                "Wrong format for percent",
+                MIN_PERCENT,
+                MAX_PERCENT).intValue();
+    }
+
+    private static long readSales(InputOutput io) {
+        return io.readNumberRange(
+                "Enter sales:",
+                "Wrong format for sales",
+                MIN_SALES,
+                MAX_SALES).longValue();
+    }
+
+}
